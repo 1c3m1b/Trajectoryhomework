ff0e7523f810b28ac4dacb6b2724459ae9c2f7f9
1c3m1b <1577442825@qq.com>
2025-01-08 22:02:06 +0800

    new version

---
+++ build/TrajectoryHomework/astar_path_planner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/recognition/Desktop/trajectoryhomework/src/TrajectoryHomework/astar_path_planner
+# Build directory: /home/recognition/Desktop/trajectoryhomework/build/TrajectoryHomework/astar_path_planner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
