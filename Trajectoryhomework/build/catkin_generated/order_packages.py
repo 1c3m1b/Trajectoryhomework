9cfef02578202fd251b4ac435ea809621a686b90
1c3m1b <1577442825@qq.com>
2025-01-08 22:22:42 +0800

    delete file

--- Trajectoryhomework/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/recognition/Desktop/trajectoryhomework/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/recognition/Desktop/trajectoryhomework/devel;/opt/ros/noetic'.split(';') if '/home/recognition/Desktop/trajectoryhomework/devel;/opt/ros/noetic' != '' else []
